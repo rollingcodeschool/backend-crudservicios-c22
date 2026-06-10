3ce929e017c2a211b8ef792b3a20cd6fa0a10a66
Emilse Arias <emilsearias@gmail.com>
2026-06-10 20:29:15 -0300

    agregamos la configuracion de los middlewares en el server

--- src/server/config.js
+++ src/server/config.js
@@ -9,11 +9,21 @@ export default class Server {
     //inicializar las propiedades del futuro objeto
     this.app = express();
     this.PORT = process.env.PORT || 3000;
+    this.middlewares()
   }
   // definir metodos
+  middlewares() {
+    this.app.use(cors()); //permite conexiones remotas
+    this.app.use(express.json()); // permite interpretar los datos que lleguen en la solicitud o request en formato json
+    this.app.use(morgan("dev"));
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    // configurar un archivo estatico como pagina principal
+    this.app.use(express.static(__dirname + "/../../public"));
+  }
+
   listen() {
     this.app.listen(this.PORT, () => {
-      console.log(`Servidor activo en http://localhost:${this.PORT}`);
+      console.info(`Servidor activo en http://localhost:${this.PORT}`);
     });
   }
 }
