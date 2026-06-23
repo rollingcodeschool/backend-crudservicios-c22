20412d691ed1361b7348e4250bdbcab762cbd54e
Emilse Arias <emilsearias@gmail.com>
2026-06-22 21:28:36 -0300

    agregamos las rutas y el controlador para editar un servicio

--- src/controllers/servicios.controllers.js
+++ src/controllers/servicios.controllers.js
@@ -57,4 +57,16 @@ export const borrarServicioPorID = async (req, res)=>{
         res.status(500).json({mensaje: 'Ocurrio un error al intentar eliminar un servicio por id'})
     }
 }
+export const editarServicioPorID = async (req, res)=>{
+    try{
+       const servicioEditado = await Servicio.findByIdAndUpdate(req.params.id, req.body, {new:true})
+       if(!servicioEditado){
+         return res.status(404).json({mensaje: 'No se encontro un servicio con el id enviado'})
+       }
+       res.status(200).json({mensaje: 'El servicio se actualizo correctamente', servicio: servicioEditado})
+    }catch(error){
+        console.error(error)
+        res.status(500).json({mensaje: 'Ocurrio un error al intentar editar un servicio por id'})
+    }
+}
 
--- src/routes/servicios.routes.js
+++ src/routes/servicios.routes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { borrarServicioPorID, buscarServicioPorID, crearServicio, listarServicios, prueba } from "../controllers/servicios.controllers.js";
+import { borrarServicioPorID, buscarServicioPorID, crearServicio, editarServicioPorID, listarServicios, prueba } from "../controllers/servicios.controllers.js";
 
 const router = Router();
 //http://localhost:3000/api/servcicios/sdfsdfsdfsd
 // get - post - put- delete -patch
 router.route('/test').get(prueba)
 router.route('/').post(crearServicio).get(listarServicios)
-router.route('/:id').get(buscarServicioPorID).delete(borrarServicioPorID)
+router.route('/:id').get(buscarServicioPorID).delete(borrarServicioPorID).put(editarServicioPorID).patch(editarServicioPorID)
 
 export default router
