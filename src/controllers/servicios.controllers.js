67a95e137321a28f64942737d0d397f0bfa24a3e
Emilse Arias <emilsearias@gmail.com>
2026-07-27 20:08:30 -0300

    agregamos un filtro de busqueda en el controlador listarServicios, el filtro busca un termino en la propiedad nombreServicio

--- src/controllers/servicios.controllers.js
+++ src/controllers/servicios.controllers.js
@@ -1,71 +1,115 @@
-import Servicio from "../models/servicio.js"
+import Servicio from "../models/servicio.js";
 
-export const prueba = (req, res)=>{
-    const vehiculos = ['🏎️', '🚗', '🚕']
-    
-    res.json({
-        mensaje: 'Bienvenidos a nuestro backend',
-        vehiculos
-    })
-}
+export const prueba = (req, res) => {
+  const vehiculos = ["🏎️", "🚗", "🚕"];
 
-export const crearServicio = async (req, res)=>{
-    try{
-        //luego agregamos la validacion
-        const servicioNuevo = new Servicio(req.body)
-        await servicioNuevo.save()
-        //ahora debo dar de alta el servicio en la BD
-        res.status(201).json({mensaje: 'El servicio fue creado correctamente'})
-    }catch(error){
-        console.error(error)
-        res.status(500).json({mensaje: 'Ocurrio un error al intentar crear un servicio'})
-    }
-}
+  res.json({
+    mensaje: "Bienvenidos a nuestro backend",
+    vehiculos,
+  });
+};
 
-export const listarServicios = async (req, res)=>{
-    try{
-       const servicios = await Servicio.find().populate('categoria', 'nombre descripcion')
-       res.status(200).json(servicios)
-    }catch(error){
-        console.error(error)
-        res.status(500).json({mensaje: 'Ocurrio un error al intentar crear un servicio'})
-    }
-}
+export const crearServicio = async (req, res) => {
+  try {
+    //luego agregamos la validacion
+    const servicioNuevo = new Servicio(req.body);
+    await servicioNuevo.save();
+    //ahora debo dar de alta el servicio en la BD
+    res.status(201).json({ mensaje: "El servicio fue creado correctamente" });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ mensaje: "Ocurrio un error al intentar crear un servicio" });
+  }
+};
 
-export const buscarServicioPorID = async (req, res)=>{
-    try{
-       const servicioBuscado = await Servicio.findById(req.params.id).populate('categoria', 'nombre descripcion')
-       if(!servicioBuscado){
-         return res.status(404).json({mensaje: 'No se encontro un servicio con el id enviado'})
-       }
-       res.status(200).json(servicioBuscado)
-    }catch(error){
-        console.error(error)
-        res.status(500).json({mensaje: 'Ocurrio un error al intentar buscar un servicio por id'})
+export const listarServicios = async (req, res) => {
+  try {
+    console.log(req.query);
+    const { termino } = req.query;
+    const query = {};
+    //verificar si tenemos algun termino de busqueda
+    if (termino) {
+      query.nombreServicio = { $regex: termino, $options: "i" };
     }
-}
-export const borrarServicioPorID = async (req, res)=>{
-    try{
-       const servicioBorrado = await Servicio.findByIdAndDelete(req.params.id)
-       if(!servicioBorrado){
-         return res.status(404).json({mensaje: 'No se encontro un servicio con el id enviado'})
-       }
-       res.status(200).json({mensaje: 'El servicio se elimino correctamente'})
-    }catch(error){
-        console.error(error)
-        res.status(500).json({mensaje: 'Ocurrio un error al intentar eliminar un servicio por id'})
-    }
-}
-export const editarServicioPorID = async (req, res)=>{
-    try{
-       const servicioEditado = await Servicio.findByIdAndUpdate(req.params.id, req.body, {new:true})
-       if(!servicioEditado){
-         return res.status(404).json({mensaje: 'No se encontro un servicio con el id enviado'})
-       }
-       res.status(200).json({mensaje: 'El servicio se actualizo correctamente', servicio: servicioEditado})
-    }catch(error){
-        console.error(error)
-        res.status(500).json({mensaje: 'Ocurrio un error al intentar editar un servicio por id'})
-    }
-}
 
+    const servicios = await Servicio.find(query).populate(
+      "categoria",
+      "nombre descripcion",
+    );
+    res.status(200).json(servicios);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ mensaje: "Ocurrio un error al intentar crear un servicio" });
+  }
+};
+
+export const buscarServicioPorID = async (req, res) => {
+  try {
+    const servicioBuscado = await Servicio.findById(req.params.id).populate(
+      "categoria",
+      "nombre descripcion",
+    );
+    if (!servicioBuscado) {
+      return res
+        .status(404)
+        .json({ mensaje: "No se encontro un servicio con el id enviado" });
+    }
+    res.status(200).json(servicioBuscado);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({
+        mensaje: "Ocurrio un error al intentar buscar un servicio por id",
+      });
+  }
+};
+export const borrarServicioPorID = async (req, res) => {
+  try {
+    const servicioBorrado = await Servicio.findByIdAndDelete(req.params.id);
+    if (!servicioBorrado) {
+      return res
+        .status(404)
+        .json({ mensaje: "No se encontro un servicio con el id enviado" });
+    }
+    res.status(200).json({ mensaje: "El servicio se elimino correctamente" });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({
+        mensaje: "Ocurrio un error al intentar eliminar un servicio por id",
+      });
+  }
+};
+export const editarServicioPorID = async (req, res) => {
+  try {
+    const servicioEditado = await Servicio.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true },
+    );
+    if (!servicioEditado) {
+      return res
+        .status(404)
+        .json({ mensaje: "No se encontro un servicio con el id enviado" });
+    }
+    res
+      .status(200)
+      .json({
+        mensaje: "El servicio se actualizo correctamente",
+        servicio: servicioEditado,
+      });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({
+        mensaje: "Ocurrio un error al intentar editar un servicio por id",
+      });
+  }
+};
