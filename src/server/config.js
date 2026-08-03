900d0f2b7523d9238d553a4954fe0bc46629ec13
Emilse Arias <emilsearias@gmail.com>
2026-08-03 17:46:32 -0300

    agregue configuracion de cors

--- src/server/config.js
+++ src/server/config.js
@@ -15,7 +15,13 @@ export default class Server {
   }
   // definir metodos
   middlewares() {
-    this.app.use(cors());
+    this.app.use(cors({
+        // Lee la variable del .env, si no existe usa localhost por defecto
+        origin: process.env.FRONTEND_URL || "http://localhost:5173", 
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+      }));
     this.app.use(express.json()); // permite interpretar los datos que lleguen en la solicitud o request en formato json
     this.app.use(morgan("dev"));
     this.app.use(cookieParser());
